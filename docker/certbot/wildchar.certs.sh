a5a8acb9a4cbc5c17e0087aaae35b89e4e8105c2
caccamo antonio <caccamo.antonio@gmail.com>
2024-03-25 18:11:18 +0100

    ADD : keycloak

--- docker/certbot/wildchar.certs.sh
+++ docker/certbot/wildchar.certs.sh
@@ -3,22 +3,22 @@
 
 # production
 
-# docker run -it --rm --name certbot \
-#    -v "./etc/letsencrypt:/etc/letsencrypt" \
-#    -v "./var/lib/letsencrypt:/var/lib/letsencrypt" \
-#    certbot/certbot certonly --manual -d *.$DOMAIN -d $DOMAIN \
-#    --agree-tos --manual-public-ip-logging-ok --preferred-challenges dns \
-#    --server https://acme-v02.api.letsencrypt.org/directory \
-#    --register-unsafely-without-email --rsa-key-size 4096
-
-
-
-# staging 
-
 docker run -it --rm --name certbot \
    -v "/etc/letsencrypt:/etc/letsencrypt" \
    -v "/var/lib/letsencrypt:/var/lib/letsencrypt" \
    certbot/certbot certonly --manual -d *.local.$DOMAIN -d local.$DOMAIN \
    --agree-tos --manual-public-ip-logging-ok --preferred-challenges dns \
-   --server https://acme-staging-v02.api.letsencrypt.org/directory \
+   --server https://acme-v02.api.letsencrypt.org/directory \
    --register-unsafely-without-email --rsa-key-size 4096
+
+
+
+# staging 
+
+# docker run -it --rm --name certbot \
+#    -v "/etc/letsencrypt:/etc/letsencrypt" \
+#    -v "/var/lib/letsencrypt:/var/lib/letsencrypt" \
+#    certbot/certbot certonly --manual -d *.local.$DOMAIN -d local.$DOMAIN \
+#    --agree-tos --manual-public-ip-logging-ok --preferred-challenges dns \
+#    --server https://acme-staging-v02.api.letsencrypt.org/directory \
+#    --register-unsafely-without-email --rsa-key-size 4096
